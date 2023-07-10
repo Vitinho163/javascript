fb95d6a3f178ab416d905b7198678ad2ae0fbb77
João Victor <94796881+Vitinho163@users.noreply.github.com>
2023-07-10 20:39:01 -0300

    Add files via upload

---
+++ OperadorString.js
@@ -0,0 +1,12 @@
+// Operador de String (String operator)
+
+// comparison (comparação)
+//console.log('a' == 'a')
+
+// concatenation (concatenação)
+// Retorna a união de duas Strings
+let alpha1 = 'alpha'
+let alpha2 = 'alpha'
+console.log(alpha1 += 'bet') 
+//or == ou
+console.log(alpha2 + 'bet')
